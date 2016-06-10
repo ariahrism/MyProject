ba9aa41254b56b02379cc9a221ec913803c726c6
truecrisis <truecrisis@gmail.com>
2016-06-10 14:16:56 +0900

    Add 'what do you want to do?'

--- app.py
+++ app.py
@@ -2,19 +2,26 @@ import os
 from bottle import route, run, request, response
 import sys
 
-
 my_env = os.getenv('my_env')
 
+
 @route('/')
-def wrike():
+def landing():
+    if unicode(request.query.get('code', ''), "utf-8") != '':
+        import wrike
+        if not wrike.creds.new_auth(unicode(request.query.get('code', ''), "utf-8")):
+            return 'Fatal Error'
+        else:
+            response.set_cookie("wrike_refresh_token", wrike.creds.refresh_token, max_age=3600)  # make expire date a dynamic duration TODO
+
+    wrikepage = "/wrike/timelogs/"
+    return 'What do you want to do? <br> <br> <a href="' + wrikepage + '">Run timelog report.</a>'
+
+
+@route('/wrike/<path>/')
+def wrike(path=''):
     try:
         import wrike
-        if unicode(request.query.get('code', ''), "utf-8") != '':
-            if not wrike.creds.new_auth(unicode(request.query.get('code', ''), "utf-8")):
-                return 'Fatal Error'
-            else:
-                response.set_cookie("wrike_refresh_token", wrike.creds.refresh_token, max_age=3600)  # make expire date a dynamic duration TODO
-
         if request.get_cookie("wrike_refresh_token"):
             print 'cookie found'
             my_cookie = request.get_cookie("wrike_refresh_token")
@@ -30,18 +37,18 @@ def wrike():
         else:
             response.set_cookie("wrike_refresh_token", wrike.creds.refresh_token, max_age=3600)  # make expire date a dynamic duration TODO
 
-        timelog_list = wrike.make_table()
+        if path == 'timelogs':
+            timelog_list = wrike.make_table()
+            import my_google
+            sheets = my_google.session
+            workbook_url = 'https://docs.google.com/spreadsheets/d/1tkjX3EL8LhftztrM0FR2gRJH6foC6zCFCHY4fdrKjVY/edit#gid=0'
+            sheets.open_workbook(workbook_url)
+            sheets.open_worksheet('Sheet2')
+            sheets.clear_sheet()
+            sheets.upload_table(timelog_list, start_row=2)
+            return 'Your sheet was updated at <a href="{}">{}</a>'.format(workbook_url, workbook_url)
 
-        import my_google
-        sheets = my_google.session
-        workbook_url = 'https://docs.google.com/spreadsheets/d/1tkjX3EL8LhftztrM0FR2gRJH6foC6zCFCHY4fdrKjVY/edit#gid=0'
-        sheets.open_workbook(workbook_url)
-        sheets.open_worksheet('Sheet2')
-        sheets.clear_sheet()
-
-        sheets.upload_table(timelog_list, start_row=2)
     except Exception as e:
-
         exc_type, exc_value, exc_traceback = sys.exc_info()
         traceback_template = '''Traceback (most recent call last):
           File "%(filename)s", line %(lineno)s, in %(name)s
@@ -56,8 +63,7 @@ def wrike():
         }
 
         return ('error\n' + traceback_template % traceback_details).replace('\n', '<br />')
-
-    return 'Your sheet was updated at <a href="{}">{}</a>'.format(workbook_url,workbook_url)
+    return '<pre>Command ', path, ' unknown</pre>'
 
 
 # if __name__ == "__main__":
